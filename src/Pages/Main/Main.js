aa3f5649fcec2f2b73f775b39391c54e03d33e01
Jorg Bernhard Reske <93399055+JorgBernhard@users.noreply.github.com>
2022-08-10 10:23:37 -0300

    background

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Background from '../../assets/background.png'
+
+function Main() {
+  return (
+    <div className="Container">
+      <img className="Background" src={Background} alt="back"></img>     
+    
+    </div>
+    
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Main from './Pages/Main/Main';
 
-import Main from './Main/Main';
-import Header from './Header/Header';
-import Portfolio from './Portfolio/Portfolio';
-import Project from './Projects/Projects';
-import Contact from './Contact/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
     <Main />
-    <Portfolio /> 
-    <Project />
-    <Contact />
   </React.StrictMode>
 );
-
