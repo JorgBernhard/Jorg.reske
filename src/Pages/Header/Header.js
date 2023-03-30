f8c358fa3a6e44baadc848d516d718c20e91944a
Jorg Bernhard Reske <93399055+JorgBernhard@users.noreply.github.com>
2023-03-30 13:31:18 -0300

    w4y update

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -9,6 +9,7 @@ function Header() {
             <a className="HeaderHome" href='#home'>Home</a>
             <a className="HeaderWork"  href='#work'>Work</a>
             <a className="HeaderEducation"  href='#education'>Education</a>
+            <a className="HeaderW4YDesign" target="_blank" rel="noreferrer"  href='http://localhost:3000/#main'>W4Y Design</a>
             <a className="HeaderContact"  href='#contact'>Contact</a>
       </div>
       </div>
